12fea272722a8d215db62511675e3e8d5091cb28
Jessica Menezes <jessicamenezes833@gmail.com>
2021-06-08 17:04:14 -0300

    Primeiro desafio

---
+++ apenas-para-maiores.js
@@ -0,0 +1,11 @@
+function solucao(idade) {
+  //seu codigo aqui
+      const int = Number(idade);
+
+      if(int >= 18) {
+        console.log('PODE ENTRAR');
+      } else {
+        console.log('ACESSO NEGADO');
+      }
+  
+}
